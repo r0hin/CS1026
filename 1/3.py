d96e6fec36d9356315d4c54e778b9d0e6c3dcb7b
Rohin <git@r0h.in>
2023-09-18 18:32:52 -0400

    Empty

--- 1/3.py
+++
@@ -1,7 +0,0 @@
-
-# Prompt user for input
-firstname = input("What is your first name? ")
-lastname = input("What is your last name? ")
-food = input("What is your favorite food? ")
-
-print("Hello, " + firstname + " " + lastname + ".\nI also like " + food + ".")
--- 1/4.py
+++
@@ -1,4 +0,0 @@
-firstname = input("What is your first name? ")
-lastname = input("What is your last name? ")
-
-print("Your initials are " + firstname[0].capitalize() + lastname[0].capitalize() + ".")
